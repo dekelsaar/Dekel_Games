9425322e6a072b1d790873dc5b1c591e5e80893d
dekelsaar <dekelsaar27@gmail.com>
2022-03-06 19:20:40 +0200

    update

--- MainScores.py
+++ MainScores.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def score_server():
+    global score_file
     try:
         score_file = open(Path("Scores.txt"), "r")
         score = score_file.read()
         return render_template('score.html', SCORE=score)
     except:
         return render_template('error.html', ERROR='Unknown Error')
+    finally:
+        score_file.close()
 
 
 app.run()
--- Score.py
+++ Score.py
@@ -2,12 +2,17 @@ from Utils import Path
 
 
 def add_score(chosen_difficulty):
+    global score
     points_of_winning = str((chosen_difficulty * 3) + 5)
 
     try:
         score = open(Path("Scores.txt"), "r")
         score = open(Path("Scores.txt"), "a")
         score.write(f" ,{points_of_winning}")
+        score.close()
     except:
         score = open(Path("Scores.txt"), "x")
         score.write(f" ,{points_of_winning}")
+        score.close()
+    finally:
+        score.close()
