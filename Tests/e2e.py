28bb6fac575377ed76531b1f5b54db09e0773f19
dekelsaar <98514603+dekelsaar@users.noreply.github.com>
2022-03-26 15:08:24 +0300

    Add files via upload

---
+++ Tests/e2e.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path="/home/dekel/Downloads/World of Games/chromedriver")
+
+
+def test_scores_service():
+    driver.get("http://127.0.0.1:5000/")
+    score_element = int(driver.find_element(by=By.ID, value="score").text)
+    print(score_element)
+    driver.close()
+    if 1000 >= score_element >= 1:
+        return True
+    else:
+        return False
+
+
+def main_function():
+    test = test_scores_service()
+    if test:
+        return 0
+    else:
+        return -1
+
+
+print(main_function())
