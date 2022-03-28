8e2a81d48266b0ed94515c0d22e6d747629a5538
dekelsaar <98514603+dekelsaar@users.noreply.github.com>
2022-03-29 00:22:32 +0300

    Update e2e.py

--- Tests/e2e.py
+++ Tests/e2e.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("chromedriver")
+driver = webdriver.Chrome("Tests/chromedriver")
 
 
 def test_scores_service():
